df8a0c02af384ea5953013654ab99abd73bb01f9
Marcel Vlasenco <marcelvlasenco@gmail.com>
2022-05-05 10:44:25 +0300

    Add google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.setUpAnalytics();
+  }
+
+  setUpAnalytics() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      //@ts-ignore
+      .subscribe((event: NavigationEnd) => {
+        gtag('config', 'G-3P3RLZDWR0', {
+          page_path: event.urlAfterRedirects,
+        });
+      });
+  }
 }
